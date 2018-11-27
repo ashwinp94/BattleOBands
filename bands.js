382ce10aefbe747912b7df8d63e118ad57109c53
Ashwin Prakash <ashp94@gmail.com>
2018-11-27 14:57:14 -0600

    final version, changed source code

--- bands.js
+++ bands.js
@@ -3,14 +3,15 @@ let bandNumber = 0
 const takeNumber = function (band) {
 
 bandNumber++;
-console.log(bandNumber, band)
+return bandNumber +" "+ band;
+
 }
-    
+
 const scum = takeNumber("Galactic Scum")
 console.log(scum)   
 
 const under = takeNumber("Underdogs")
 console.log(under)   
 
-const bad = takeNumber("dogs")
-console.log(bad)
+const good = takeNumber("Dogs")
+console.log(good)
